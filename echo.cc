4d53b4b74843258cd39567cb5b0cd393748f3979
iphuck22 <eldeshue@naver.com>
2022-10-01 00:16:06 +0900

    add echo.cc source file

---
+++ echo.cc
@@ -0,0 +1,12 @@
+// home work of OSS, echo program. 
+// return input string, written in C++
+#include<iostream>
+#include<string>
+
+int main(){
+	std::string a;
+	std::cout<<"Type a string you want to echoing"<<std::endl;
+	std::cin>> a ;
+	std::cout<< a <<std::endl;
+	return 0;
+}
